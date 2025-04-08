3e2b18c109e324ce0e7a791808057a04d7ad9ab2
Rodmtdk <rod.mtdk.gng@gmail.com>
2025-04-08 20:59:59 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = 'nygx-cache-v1';
+const filesToCache = [
+  'index.html',
+  'connexion.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
