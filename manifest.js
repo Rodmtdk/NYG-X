f15d3a97fd712d2ca98eae60cfe3bf2b2024ec23
Rodmtdk <rod.mtdk.gng@gmail.com>
2025-04-08 20:59:29 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "NYG-X",
+  "short_name": "NYG-X",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0b1120",
+  "theme_color": "#0ff",
+  "description": "NYG-X Dashboard",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
